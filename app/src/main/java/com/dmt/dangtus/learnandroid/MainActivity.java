ceb81b209a8617285a05824dfccedf50df12b81f
DangTus <dangtu9520@gmail.com>
2022-09-07 08:32:26 +0700

    swap activity

--- app/src/main/java/com/dmt/dangtus/learnandroid/MainActivity.java
+++ app/src/main/java/com/dmt/dangtus/learnandroid/MainActivity.java
@@ -2,13 +2,28 @@ package com.dmt.dangtus.learnandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSwap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSwap = (Button) findViewById(R.id.buttonSwap);
+
+        btnSwap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
     }
 }
