05ac031758f49791678d587c8bacc5fc63cb19ac
DangTus <dangtu9520@gmail.com>
2022-09-19 21:04:02 +0700

    them chuc nang bao loi (trang sign up)

--- app/src/main/java/com/dmt/dangtus/learnandroid/SignUpActivity.java
+++ app/src/main/java/com/dmt/dangtus/learnandroid/SignUpActivity.java
@@ -1,9 +1,13 @@
 package com.dmt.dangtus.learnandroid;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +17,7 @@ import android.widget.Toast;
 public class SignUpActivity extends AppCompatActivity {
 
     TextView txtLogin;
-    EditText edtName, edtUserName, edtPassword, getEdtPasswordConfirm;
+    EditText edtName, edtUserName, edtPassword, edtEdtPasswordConfirm;
     Button btnSignUp;
 
     @Override
@@ -34,19 +38,95 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intentSignUp = new Intent(SignUpActivity.this, MainActivity.class);
-//                startActivity(intentSignUp);
+
                 checkEmpty(edtName);
+                checkUserName();
+                checkPassword();
+                checkPasswordConfirm();
+
+                if(edtName.getError() == null && edtUserName.getError() == null && edtPassword.getError() == null && edtEdtPasswordConfirm.getError() == null) {
+                    showSuccessDialog();
+                } else {
+                    showErrorDialog();
+                }
+            }
+        });
+    }
+
+    private boolean checkEmpty(EditText edtDuLieu) {
+        if(edtDuLieu.getText().toString().trim().isEmpty()) {
+            edtDuLieu.setError("Không được để trống ô này");
+            edtDuLieu.setBackgroundResource(R.drawable.background_input_text_error);
+            return true;
+        } else {
+            edtDuLieu.setError(null);
+            edtDuLieu.setBackgroundResource(R.drawable.background_input_text);
+            return false;
+        }
+    }
+
+    private void checkUserName() {
+        if(!checkEmpty(edtUserName)) {
+            if(edtUserName.getText().toString().trim().length() < 8) {
+                edtUserName.setError("Vui lòng nhập đủ 8 kí tự");
+                edtUserName.setBackgroundResource(R.drawable.background_input_text_error);
+            }
+        }
+    }
+
+    private void checkPassword() {
+        if(!checkEmpty(edtPassword)) {
+            if(edtPassword.getText().toString().trim().length() < 8) {
+                edtPassword.setError("Vui lòng nhập đủ 8 kí tự");
+                edtPassword.setBackgroundResource(R.drawable.background_input_text_error);
+            }
+        }
+    }
+
+    private void checkPasswordConfirm() {
+        if(!checkEmpty(edtEdtPasswordConfirm)) {
+            String password = edtPassword.getText().toString().trim();
+            String passwordConfirm = edtEdtPasswordConfirm.getText().toString().trim();
+            if(!password.equals(passwordConfirm)) {
+                edtEdtPasswordConfirm.setError("Mật khẩu bạn vừa nhập không đúng");
+                edtEdtPasswordConfirm.setBackgroundResource(R.drawable.background_input_text_error);
+            }
+        }
+    }
+
+    private void showSuccessDialog() {
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Thông báo!");
+        alertBuilder.setIcon(R.mipmap.ic_launcher);
+        alertBuilder.setMessage("Đăng kí thành công, vui lòng đăng nhập lại để truy cập hệ thống");
+        alertBuilder.setCancelable(false);
+
+        alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intentSignUp = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intentSignUp);
             }
         });
 
-
+        alertBuilder.show();
     }
 
-    private void checkEmpty(EditText edtDuLieu) {
-        if(edtDuLieu.getText().toString().trim().isEmpty()) {
-            edtDuLieu.setError("Vui lòng nhập");
-        }
+    private void showErrorDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Lỗi!");
+        alertBuilder.setIcon(R.mipmap.ic_launcher);
+        alertBuilder.setMessage("Đăng kí không thành công, vui lòng kiểm tra các lỗi và thử lại");
+        alertBuilder.setCancelable(false);
+
+        alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        alertBuilder.show();
     }
 
     private void anhXa() {
@@ -56,6 +136,6 @@ public class SignUpActivity extends AppCompatActivity {
         edtName = (EditText) findViewById(R.id.fullNameEditText);
         edtUserName = (EditText) findViewById(R.id.userNameEditText);
         edtPassword = (EditText) findViewById(R.id.passwordEditText);
-        getEdtPasswordConfirm = (EditText) findViewById(R.id.confirmPasswordEditText);
+        edtEdtPasswordConfirm = (EditText) findViewById(R.id.confirmPasswordEditText);
     }
 }
