cf9edc2168d8ec39b3b45ff8420dfeeae22d9ab2
DangTus <dangtu9520@gmail.com>
2022-09-24 16:12:14 +0700

    them thanh cong bottom nav

--- app/src/main/java/com/dmt/dangtus/learnandroid/ProfileActivity.java
+++ app/src/main/java/com/dmt/dangtus/learnandroid/ProfileActivity.java
@@ -1,11 +1,17 @@
 package com.dmt.dangtus.learnandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -13,6 +19,7 @@ import java.util.ArrayList;
 public class ProfileActivity extends AppCompatActivity {
 
     Button btnLogOut;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,23 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         anhXa();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        break;
+                    case R.id.action_listview:
+                        break;
+                    case R.id.action_profile:
+                        break;
+                    case R.id.action_setting:
+                        break;
+                }
+                return true;
+            }
+        });
 
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +56,6 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void anhXa() {
         btnLogOut = (Button) findViewById(R.id.logOutButton);
+        bottomNavigationView = findViewById(R.id.bottomNav);
     }
 }
