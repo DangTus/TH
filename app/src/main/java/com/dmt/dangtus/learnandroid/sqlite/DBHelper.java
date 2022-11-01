3bf98d8418032cd02b3b5ecef820baf4e4f1b593
DangTus <dangtu9520@gmail.com>
2022-11-01 15:48:39 +0700

    hien computer

--- app/src/main/java/com/dmt/dangtus/learnandroid/sqlite/DBHelper.java
+++ app/src/main/java/com/dmt/dangtus/learnandroid/sqlite/DBHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DB_NAME = "demo";
+    public static final String DB_NAME = "quanlimaytinh";
     public static final int DB_VERSION = 1;
 
     public DBHelper(Context context) {
@@ -17,14 +17,42 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "CREATE TABLE SinhVien (id text primary key, name text not null, score text not null)";
+        //tao bang danh muc
+        String sql =    "CREATE TABLE DanhMuc " +
+                        "(id int primary key, " +
+                        "ten_danhmuc nvarchar(50) not null)";
+        sqLiteDatabase.execSQL(sql);
+
+        //them du lieu vao bang danh muc
+        sql =   "INSERT INTO DanhMuc " +
+                "VALUES(1, 'Máy tính văn phòng'), " +
+                "(2, 'Máy tính gamming')";
+        sqLiteDatabase.execSQL(sql);
+
+        //tao bang may tinh
+        sql =   "CREATE TABLE MayTinh " +
+                "(id int primary key, " +
+                "ten_maytinh nvarchar(50) not null, " +
+                "id_danhmuc int not null, " +
+                "FOREIGN KEY (id_danhmuc) REFERENCES DanhMuc (id))";
+        sqLiteDatabase.execSQL(sql);
+
+        //them du lieu vao bang may tinh
+        sql =   "INSERT INTO MayTinh " +
+                "VALUES(1, 'Dell 1', 1), " +
+                "(2, 'Dell 2', 2), " +
+                "(3, 'Dell 3', 1)";
         sqLiteDatabase.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        String sql = "DROP TABLE IF EXISTS SinhVien";
+        String sql = "DROP TABLE IF EXISTS MayTinh";
         sqLiteDatabase.execSQL(sql);
+
+        sql = "DROP TABLE IF EXISTS DanhMuc";
+        sqLiteDatabase.execSQL(sql);
+
         onCreate(sqLiteDatabase);
     }
 }
